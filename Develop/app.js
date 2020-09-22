b462fc50dc9b3936d2ebb1296a5a14db6f10bfa8
syed dilawar <wasimdilawer@gmail.com>
2020-09-22 13:00:35 -0700

    working on app js file

--- Develop/app.js
+++ Develop/app.js
@@ -6,6 +6,44 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const employees = [];
+
+inquirer
+  .prompt([
+    
+    {
+      type: "input",
+      message: "Please enter the Managers name:",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "What is the Managers id?",
+      name: "managerId",
+    },
+    {
+      type: "input",
+      message: "What is the Managers email?",
+      name: "managerEmail",
+    },
+    {
+      type: "input",
+      message: "What is the Managers office number?",
+      name: "managerOfficeNumber",
+    },
+    //engineer questions
+    {
+      type: "list",
+      message: "Would you like to input an engineer?",
+      choices: ["yes", "no"],
+      name: "engineerYesNo",
+    },
+  ])
+  .then((answers) => {
+    //manager answers
+    const manager = new Manager (answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
+    employees.push(manager);
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 

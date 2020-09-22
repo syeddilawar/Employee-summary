3eab7166db9ee4ae18933cb8d1e52ce8b2ed5a11
syed dilawar <wasimdilawer@gmail.com>
2020-09-22 12:17:33 -0700

    html done

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,5 +1,5 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
 const Employee = require("./Employee");
 class Intern extends Employee {
   constructor(name, id, email, school) {
